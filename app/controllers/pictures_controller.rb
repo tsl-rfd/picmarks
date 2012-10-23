d954548b450b68cb98b4657153e24b5fd3efef4c
Raghu Betina <raghu@starterleague.com>
2012-10-23 16:42:52 -0500

    Add create and destroy actions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,24 +2,25 @@ class PicturesController < ApplicationController
   def new
   end
   
+  def create
+    p = Picture.new
+    p.url = params[:url]
+    p.note = params[:note]
+    p.save
+    redirect_to "/pictures"
+  end
+  
+  def destroy
+    p = Picture.find_by_id(params[:id])
+    p.destroy
+    redirect_to "/pictures"
+  end
+  
   def index
-    if params[:url]  
-      p = Picture.new
-      p.url = params[:url]
-      p.save
-    end
     @pictures = Picture.all
-    
   end
   
   def show
-    # This was our old code from when we had hardcoded data.
-    # id_number = params[:id].to_i
-    # @url = @photo_urls[id_number - 1]
-    
-    # Use the Picture.find_by_id method to make the show action work.
-    # You may also have to update the view template.
-    
-    @pic # = ???
+    @pic = Picture.find_by_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Picmarks::Application.routes.draw do
   
   get '/pictures', :controller => 'pictures', :action => 'index'
+  post '/pictures', :controller => 'pictures', :action => 'create'
+  delete '/pictures/:id', :controller => 'pictures', :action => 'destroy'
   
   get '/pictures/new', :controller => 'pictures', :action => 'new'
   
